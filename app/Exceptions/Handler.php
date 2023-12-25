53e9e53e4ea81a760055e502cbf8e16f3ef37ae0
WillianAbrego <willianantonioabrego@gmail.com>
2023-12-25 16:46:40 -0600

    sobreescritura de metodo filedAuthorization por jsonauthorizationexception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -22,7 +22,8 @@ class Handler extends ExceptionHandler
      * @var array<int, class-string<\Throwable>>
      */
     protected $dontReport = [
-        AuthenticateException::class
+        AuthenticateException::class,
+        JsonAuthorizationException::class
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::post('login', [LoginController::class, 'authenticate']);
+
+Route::get('user/index', [UserController::class, 'index']);
