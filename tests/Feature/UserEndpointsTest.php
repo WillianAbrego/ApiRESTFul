4c0d2a7df3165b428dfb791c8d731605660f3057
WillianAbrego <willianantonioabrego@gmail.com>
2023-12-27 14:51:29 -0600

    test para create endpoint

--- tests/Feature/UserEndpointsTest.php
+++ tests/Feature/UserEndpointsTest.php
@@ -8,6 +8,7 @@ use Tests\TestCase;
 
 class UserEndpointsTest extends TestCase
 {
+    use WithFaker;
     /**
      * A basic feature test example.
      *
@@ -21,5 +22,21 @@ class UserEndpointsTest extends TestCase
         ];
         $payload = [];
         $this->json('GET', '/api/user/index', $payload, $headers)->assertStatus(200);
+    }
+
+    public function test_user_create_endpoint()
+    {
+        $headers = [
+            'Authorization' => config('test.token')
+
+        ];
+        $payload = [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'password_confirmation' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+
+        ];
+        $this->json('POST', '/api/user/create', $payload, $headers)->assertStatus(201);
     }
 }
